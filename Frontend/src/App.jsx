6510751afbfb0efd503d1b1179509e7ff98c767d
Pranavjha2004 <jhapranav2004@gmail.com>
2026-02-24 23:36:12 +0530

    Setup Tailwind Css and folder structure

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Feed from "./pages/Feed";
+import Profile from "./pages/Profile";
 
-const App = () => {
+import './App.css'
+
+function App() {
   return (
-    <div>
-      <p>Hello World</p>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/feed" element={<Feed />} />
+        <Route path="/profile/:id" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
